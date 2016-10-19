f096ace692eb66c305f95487f91c137165ef01b0
marcoscanton <46453895j@iespoblenou.org>
2016-10-19 19:59:20 +0200

    Creem un ArrayAdapter<String> con los titulos y los conectamos al ListView.

--- app/src/main/java/com/example/a46453895j/cartas/CartasFragment.java
+++ app/src/main/java/com/example/a46453895j/cartas/CartasFragment.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class CartasFragment extends Fragment {
+
+    private ArrayList<String> items = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
 
     public CartasFragment() {
     }
@@ -20,6 +27,24 @@ public class CartasFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_cartas, container, false);
         ListView lvCartas = (ListView) view.findViewById(R.id.LvCartas);
+
+        String[] data = {
+                "Los 400 golpes",
+                "El odio",
+                "El padrino",
+                "El padrino. Parte II",
+                "Ocurrió cerca de su casa",
+                "Infiltrados",
+                "Umberto D."
+        };
+
+        //con el metodo asList podemos introducir dentro de un array dinamica mas de una string a traves de un array de strings
+        items = new ArrayList<>(Arrays.asList(data));
+
+        //el adaptador estará formado por getcontext,por el layout que repetiremos por item, por el texview que con tiene los datos y por los datos de cada item
+        adapter = new ArrayAdapter<>(getContext(), R.layout.titulo_cartas, R.id.TvTitulos, items);
+        lvCartas.setAdapter(adapter);
+
         return view;
     }
 }
