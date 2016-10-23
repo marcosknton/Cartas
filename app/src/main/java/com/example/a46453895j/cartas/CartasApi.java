56602f51d0a95e658aa47c1912c0fe84de9d1f42
marcosknton <marcosknton@gmail.com>
2016-10-23 18:09:57 +0200

    construcción de métodos y clases para la conexión a internet y descargar los 10 primeros titulos

--- app/src/main/java/com/example/a46453895j/cartas/CartasApi.java
+++ app/src/main/java/com/example/a46453895j/cartas/CartasApi.java
@@ -16,41 +16,40 @@ import java.util.ArrayList;
 
 public class CartasApi {
 
-    private final String BASE_URL = "https://api.magicthegathering.io/v1/cards?pageSize=50";
+    private  String url= "https://api.magicthegathering.io/v1/cards?pageSize=50";
 
+    /**
+     *
+     * @return devuelve un array con todos los objetos Ocarta
+     */
+    public ArrayList<Ocarta> getOcartas() {
 
-    public ArrayList<String> getOcartas() {
-
-        //Uri builtUri = Uri.parse(BASE_URL).buildUpon().appendPath("name").build();
-
-        //String url = builtUri.toString();
-
-        final String url="https://api.magicthegathering.io/v1/cards?page=5&pageSize=100";
-
-        ArrayList<String> lista = new ArrayList<>();
-
+        ArrayList<Ocarta> lista = new ArrayList<>();
         try {
             String JsonResponse = HttpUtils.get(url);
             //creamos un objeto json y como contenido le introducimos el string resultado de la pagina
             JSONObject json = new JSONObject(JsonResponse);
-            //en un variable JSONArray le asignamos el resultado de aplocar el metodo getJSONArray al objeto json
+            //creamos un array de cada elemento que forma parte de la estrucutra del objeto json ,guiandonos por el elemento que
+            //envueleve toda la estructura que es cards
             JSONArray jsoncartas = json.getJSONArray("cards");
             String titulo;
-
-
+            //recorremos el array de elementos, extraemos la info que nos interesa y la introducimos como atributo
+            //dentro el objeto Ocarta, que posteriormente guardamos uno por uno en el array de objetos Ocarta
             for (int i = 0; i < jsoncartas.length(); ++i) {
                 JSONObject object = jsoncartas.getJSONObject(i);
                 titulo = object.getString("name");
-                lista.add(titulo);
+                Ocarta carta=new Ocarta(titulo);
+                lista.add(carta);
             }
 
+        }
 
-
-        } catch (JSONException e) {
+        catch (JSONException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return lista;
     }
+
 }
--- app/src/main/java/com/example/a46453895j/cartas/CartasFragment.java
+++ app/src/main/java/com/example/a46453895j/cartas/CartasFragment.java
@@ -14,7 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.os.AsyncTask;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -23,7 +23,7 @@ import java.util.Arrays;
  */
 public class CartasFragment extends Fragment {
 
-    private ArrayList<String> items ;
+    private ArrayList<String> items;
     private ArrayAdapter<String> adapter;
 
     public CartasFragment() {
@@ -40,11 +40,9 @@ public class CartasFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_cartas, container, false);
+        View view = inflater.inflate(R.layout.fragment_cartas, container, false);
         ListView lvCartas = (ListView) view.findViewById(R.id.LvCartas);
 
-        CartasApi resultado=new CartasApi();
-        //items=resultado.getOcartas();
 
         String[] data = {
                 "carta 1",
@@ -54,7 +52,7 @@ public class CartasFragment extends Fragment {
 
         };
         //con el metodo asList podemos introducir dentro de un array dinamica mas de una string a traves de un array de strings
-       items = new ArrayList<>(Arrays.asList(data));
+        items = new ArrayList<>(Arrays.asList(data));
 
         //el adaptador estará formado por getcontext,por el layout que repetiremos por item, por el texview que con tiene los datos y por los datos de cada item
         adapter = new ArrayAdapter<>(getContext(), R.layout.titulo_cartas, R.id.TvTitulos, items);
@@ -64,24 +62,52 @@ public class CartasFragment extends Fragment {
         return view;
     }
 
-
+    //inflamos el menu_cartas
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.menu_cartas,menu);
+        inflater.inflate(R.menu.menu_cartas, menu);
     }
-
+    //habilitamos el menuitem con las funciones que vaya a realizar
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.MiRefresh){
-            Snackbar.make(getView(),"mensaje",Snackbar.LENGTH_LONG).show();
+        if (item.getItemId() == R.id.MiRefresh) {
+            Snackbar.make(getView(), "actualizando...", Snackbar.LENGTH_LONG).show();
+            refresh();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public void refresh(){
+    public void refresh() {
+        RefreshAsyncTask refreshAsyncTask = new RefreshAsyncTask();
+        refreshAsyncTask.execute();
+    }
 
+    //Clase que extiende de AsynTask, la cual necesitaremos para trabajar en 2 plano, ya que en primer plano si se demora
+    //la ejecución en el tiempo nos fallará la conexión
+    class RefreshAsyncTask extends AsyncTask<Void, Void, ArrayList<Ocarta>> {
+        @Override
+        //devuelve un array list de objetos Ocarta() para trabajar en 2 plano
+        protected ArrayList<Ocarta> doInBackground(Void... voids) {
+            CartasApi api = new CartasApi();
+            ArrayList<Ocarta> cards = api.getOcartas();
+            /*
+            for (int i = 0; i < cards.size(); ++i) {
+                Log.d("DEBUG", cards.get(i).toString());
+            }
+            */
+            return cards;
+        }
+    //Como el método doInBackground no devuelve datos a la interfaz necesitamos del método OnPostExecute para recoger los datos
+    //de este metodo , extraer en este caso el titulo y añadirlo al adapter, que cargara los datons en el layout.
+        @Override
+        protected void onPostExecute(ArrayList<Ocarta> cards) {
+            adapter.clear();
+            for (int i = 0; i < cards.size(); ++i) {
+                adapter.add(cards.get(i).getTitulo());
+            }
+        }
     }
 
 }
